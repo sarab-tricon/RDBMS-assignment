e916c64129c84fb1b7837c0f7faceac790c78841
sarab-tricon <sarabjeet.singh@triconinfotech.com>
2025-11-05 17:22:57 +0530

    Create task5.sql

---
+++ task5.sql
@@ -0,0 +1,5 @@
+5. Create insert queries to add around 4 to 8 rows in both the tables
+INSERT INTO author (name, country) VALUES
+('J.K. Rowling', 'UK'),
+('George Orwell', 'UK'),
+('J.R.R. Tolkien', 'UK');
